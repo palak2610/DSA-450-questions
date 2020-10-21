d087ccfbe5345e5bcf5a9b987c86623783f3632b
Palak <mishra.palak2610@gmail.com>
2020-10-21 11:58:54 +0530

    Create Union of two arrays.java

---
+++ Array/Union of two arrays/Union of two arrays.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		
+		while(t > 0) {
+		    
+		    int n = scanner.nextInt();
+		    int m = scanner.nextInt();
+		    
+             HashSet<Integer> hset = new HashSet<>();
+		    
+		    for(int i = 0; i < n+m; i++) {
+		        int num = scanner.nextInt();
+		        
+		        if(hset.contains(num)) {
+		            continue;
+		        }else {
+		            hset.add(num);
+		        }
+		        
+		    }
+		     
+	      System.out.println(hset.size()); 
+		    
+		    t--;
+		}
+		
+	}
+}
